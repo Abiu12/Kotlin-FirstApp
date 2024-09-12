a1979e91a5af89d5d216c5234bb0ba665740d9e1
Abiu12 <callejas195@gmail.com>
2024-09-12 17:23:23 -0600

    Show message of welcome in ResultActivity

--- app/src/main/java/com/example/firstapp/ResultActivity.kt
+++ app/src/main/java/com/example/firstapp/ResultActivity.kt
@@ -1,37 +1,24 @@
 package com.example.firstapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class FirstAppActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_first_app)
+        setContentView(R.layout.activity_result)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnStart = findViewById<AppCompatButton>(R.id.btn)
-        val etName = findViewById<EditText>(R.id.edtext)
-        btnStart.setOnClickListener{
-            if(!etName.text.toString().isEmpty()){
-                val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra("NAME",etName.text.toString())
-                startActivity(intent)
-            }
-        }
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        var name:String = intent.extras?.getString("NAME").orEmpty()
+        tvResult.text = "Hola $name"
     }
 }
-
-
-
-
